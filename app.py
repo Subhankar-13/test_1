66d4ab807a15c499a45f14d83e3b52c9f1936109
Subhankar Panda <40689983+Subhankar-13@users.noreply.github.com>
2023-10-15 02:20:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import pickle
 from langchain.vectorstores import FAISS
 from langchain.embeddings import HuggingFaceEmbeddings
 import requests
+import subprocess
 
 st.set_page_config(page_title="NBDT Recommendation Engine Arena")
 
@@ -36,40 +37,21 @@ def download_file(url, local_filename):
 
 # URLs of your remote files
 article_list_url = "https://huggingface.co/spaces/atrytone/ArenaTester/resolve/main/article_list.pkl"
-db_1 = "https://huggingface.co/spaces/atrytone/ArenaTester/resolve/main/miread_large/index.faiss"
-db_2 = "https://huggingface.co/spaces/atrytone/ArenaTester/resolve/main/miread_contrastive/index.faiss"
-db_3 = "https://huggingface.co/spaces/atrytone/ArenaTester/resolve/main/scibert_contrastive/index.faiss"
-
-def create_directory(path):
-    if not os.path.exists(path):
-        os.makedirs(path)
-
-# Directories for the faiss files
-create_directory("db_1")
-create_directory("db_2")
-create_directory("db_3")
-
-# Local paths where the files will be downloaded
-article_list_path = "article_list_2.pkl"
-db_1_path = "db_1/index.faiss"
-db_2_path = "db_2/index.faiss"
-db_3_path = "db_3/index.faiss"
-
-
 
 # Download the files
 download_file(article_list_url, article_list_path)
-download_file(db_1, db_1_path)
-download_file(db_2, db_2_path)
-download_file(db_3, db_3_path)
 
+cmd = ["git", "clone", "https://huggingface.co/spaces/atrytone/ArenaTester"]
+
+# Run the command
+subprocess.run(cmd)
 
 
 # Now load the files from the local paths
 with open(article_list_path, "rb") as articles:
     article_list = tuple(pickle.load(articles))
     
-INDEXES = [db_1_path, db_2_path , db_3_path]
+INDEXES = ["miread_large", "miread_contrastive", "scibert_contrastive"]
 MODELS = [
     "biodatlab/MIReAD-Neuro-Large",
     "biodatlab/MIReAD-Neuro-Contrastive",
